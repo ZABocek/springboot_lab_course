b5d7223d1e20c028013b65acc60b13e28ca26231
zaboc <zaboc@localhost>
2025-12-05 14:45:23 +0100

    Complete module 36-mvc: Spring MVC REST controller implementation
    - TODO-05: Implemented accountList() to return all accounts via GET /accounts
    - TODO-08: Implemented accountDetails() to return single account via GET /accounts/{entityId}
    - TODO-12: Configured server.port=8088 in application.properties
    - All tests pass: 2 tests run, 0 failures, 0 errors
    - Verified endpoints work correctly:
      * GET /accounts returns list of all accounts in JSON
      * GET /accounts/{entityId} returns individual account details for IDs 0-20
      * Application listens on configured port 8088

--- lab/36-mvc/src/main/java/accounts/web/AccountController.java
+++ lab/36-mvc/src/main/java/accounts/web/AccountController.java
@@ -1,7 +1,9 @@
 package accounts.web;
 
 import accounts.AccountManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import rewards.internal.account.Account;
 
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.List;
  *
  */
 // TODO-03: Add an appropriate annotation to make this class a REST controller
-	
+@RestController
 public class AccountController {
 
 	private final AccountManager accountManager;
@@ -26,7 +28,6 @@ public class AccountController {
 	/**
 	 * Creates a new AccountController with a given account manager.
 	 */
-	@Autowired
 	public AccountController(AccountManager accountManager) {
 		this.accountManager = accountManager;
 	}
@@ -35,38 +36,25 @@ public class AccountController {
 	 * Return a list of all accounts
 	 */
 	// TODO-04: Add an appropriate annotation to make this method handle "/accounts"
-
+	@GetMapping("/accounts")
 	public List<Account> accountList() {
 
-		// TODO-05: Implement the logic to find and return all accounts
-		// - Use "accountManger" object to get all accounts
-		// - Recompile this class if necessary, and wait for the application to restart (via devtools)
-		// - From the home page, click the link - this should now work
-		// - If you prefer, access http://localhost:8080/accounts using curl or Postman
+		// TODO-05: Implement the logic to find and return all accounts. Use "accountManger" object to get all accounts. Recompile this class if necessary, and wait for the application to restart (via devtools). From the home page, click the link - this should now work. If you prefer, access http://localhost:8080/accounts using curl or Postman.
 
-		return null; // REPLACE THIS LINE to return a list accounts
+		return accountManager.getAllAccounts();
 		
-		// TODO-06: (If you are using STS) We are about to make lots of
-		//          changes, so stop the application otherwise Devtools
-		//          will keep restarting it.
+		// TODO-06: (If you are using STS) We are about to make lots of changes, so stop the application otherwise Devtools will keep restarting it.
+	}
+	/**
+	 * Return a single account by ID
+	 */
+	@GetMapping("/accounts/{entityId}")
+	public Account accountDetails(@PathVariable int entityId) {
+		return accountManager.getAccount((long) entityId);
 	}
 
-	// TODO-08: Implement the /accounts/{entityId} request handling method.
-	// - Call the method accountDetails().
-	// - Annotate to define URL mapping /accounts/{entityId}
-	//   this method will respond to.
-	// - Use a method parameter to obtain the URI template parameter
-	//   needed to retrieve an account.
-	// - Use the accountManager to obtain an account. This is the value to return
-	// - Save all work.
+	// TODO-10b: If AccountControllerTests.testHandleDetailsRequest() fails, fix errors before moving on.
 
-
-	// TODO-10b: If AccountControllerTests.testHandleDetailsRequest()
-	//  fails, fix errors before moving on
-
-	// TODO-11: Run the application
-	// - You should now be able to invoke http://localhost:8080/accounts/N
-	//   where N is 0-20 and get a response. You can use curl, Postman or
-	//   your browser to do this.
+	// TODO-11: Run the application. You should now be able to invoke http://localhost:8080/accounts/N, where N is 0 through 20 and get a response. You can use curl, Postman or your browser to do this.
 
 }
--- lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
+++ lab/36-mvc/src/test/java/accounts/web/AccountControllerTests.java
@@ -2,7 +2,6 @@ package accounts.web;
 
 import accounts.internal.StubAccountManager;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import rewards.internal.account.Account;
 
@@ -16,19 +15,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  */
 public class AccountControllerTests {
 
-	private static final long expectedAccountId = StubAccountManager.TEST_ACCOUNT_ID;
-	private static final String expectedAccountNumber = StubAccountManager.TEST_ACCOUNT_NUMBER;
+	private static final long EXPECTED_ACCOUNT_ID = StubAccountManager.TEST_ACCOUNT_ID;
+	private static final String EXPECTED_ACCOUNT_NUMBER = StubAccountManager.TEST_ACCOUNT_NUMBER;
 
 	private AccountController controller;
 
 	@BeforeEach
-	public void setUp() throws Exception {
+	public void setUp() {
 		controller = new AccountController(new StubAccountManager());
 	}
 
 	// TODO-07: Remove the @Disabled annotation, run the test, it should now pass.
 	@Test
-	@Disabled
 	public void testHandleListRequest() {
 		List<Account> accounts = controller.accountList();
 
@@ -38,22 +36,18 @@ public class AccountControllerTests {
 
 		// Validate that account
 		Account account = accounts.getFirst();
-		assertEquals(expectedAccountId, (long) account.getEntityId());
-		assertEquals(expectedAccountNumber, account.getNumber());
+		assertEquals(EXPECTED_ACCOUNT_ID, (long) account.getEntityId());
+		assertEquals(EXPECTED_ACCOUNT_NUMBER, account.getNumber());
 	}
 
 	// TODO-10a: Remove the @Disabled annotation, run the test, it should pass.
 	@Test
-	@Disabled
 	public void testHandleDetailsRequest() {
-		// TODO-09a: Implement test code which calls the accountDetails() method on the controller.
-		// - It will take one parameter - use "expectedAccountId" defined above
-		// - It will return an Account
+		Account account = controller.accountDetails((int) EXPECTED_ACCOUNT_ID);
 
-		// TODO-09b: Define the following assertions:
-		// - The account is not null
-		// - The account id matches "expectedAccountId" defined above
-		// - The account number matches "expectedAccountNumber" defined above
+		assertNotNull(account);
+		assertEquals(EXPECTED_ACCOUNT_ID, (long) account.getEntityId());
+		assertEquals(EXPECTED_ACCOUNT_NUMBER, account.getNumber());
 	}
 
 }
